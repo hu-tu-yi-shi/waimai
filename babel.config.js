265c1e4b0931a2bd521e6ef8b4b0542ca2eda406
hutu <104620424+hu-tu-yi-shi@users.noreply.github.com>
2022-06-26 17:44:21 +0800

    fix: ts paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ["module-resolver", { // for a bug https://gist.github.com/ChronSyn/ffca12630eb36c71465a1c5d7149a6ae
+        "alias": {
+          "@@": "./src",
+        },
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+        ]
+      }],
+    ],
   };
 };
