593ec85549a58f9848366c2639269c3aaf335fd2
hutu <104620424+hu-tu-yi-shi@users.noreply.github.com>
2022-06-26 19:23:37 +0800

    fix: 引入状态管理库 recoil

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { RecoilRoot  } from 'recoil';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,8 +15,10 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <RecoilRoot>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </RecoilRoot>
       </SafeAreaProvider>
     );
   }
