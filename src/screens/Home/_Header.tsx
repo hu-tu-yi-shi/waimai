b9521b02ed67ff0ebe035a2ea7931ead53460d9b
hutu <104620424+hu-tu-yi-shi@users.noreply.github.com>
2022-06-27 21:35:41 +0800

    fix: 首页 地址按钮 点击事件 到地址选择页面

--- src/screens/Home/_Header.tsx
+++ src/screens/Home/_Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Flex, IconButton,View} from "native-base";
+import {Box, Button, Flex, IconButton, View} from "native-base";
 import {Ionicons} from "@expo/vector-icons";
 import {Text} from "@@/components/Themed";
 import {useNavigation} from "@react-navigation/native";
@@ -9,10 +9,11 @@ const _Header = () => {
     const navigation = useNavigation();
     return (
        <Flex direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
-           <Flex direction={"row"} alignItems={"center"}>
-               <Ionicons name="location-outline" size={20} color="black" />
-               <Text>万达广场1 &gt;</Text>
-           </Flex>
+           <Button onPress={()=>{
+               navigation.navigate("SelectLocation")
+           }} variant="unstyled" leftIcon={ <Ionicons name="location-outline" size={20} color="black" />}>
+              万达广场1 &gt;
+           </Button>
 
            <IconButton onPress={()=>{
                navigation.navigate("Notification")
